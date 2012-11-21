64ab8af9b785017ed510c40fc214eb077f99c812
Dennis Hoppe <dennis.hoppe@debian-solutions.de>
2012-11-21 16:57:43 +0100

    Remove support for Maverick    - End of Life on 2012-04-12

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class role::params {
   case $::lsbdistcodename {
-    'squeeze', 'maverick', 'natty': {
+    'squeeze', 'natty': {
     }
     default: {
       fail("Module ${module_name} does not support ${::lsbdistcodename}")
